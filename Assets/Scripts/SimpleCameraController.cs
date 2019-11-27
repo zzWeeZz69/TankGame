1c89b384ff2bbbec9d13fe6717cca2efd76c174f
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2019-11-27 09:13:54 +0100

    just pulling shit

--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -3,6 +3,10 @@
 
 public class SimpleCameraController : MonoBehaviour
 {
-    
+    public struct Vars
+    {
+        GameObject Player1, player2;
+        public float ZoomSmootherner;
+    }
 
 }
