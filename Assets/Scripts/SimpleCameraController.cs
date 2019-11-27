70a11f0b373dfc82de1f879567806ca03c3c0bb2
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2019-11-27 10:07:24 +0100

    yeet

--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEngine;
+using System;
+using System.Diagnostics;
 
 
 public class SimpleCameraController : MonoBehaviour
 {
-    public struct Vars
+    
+    private void Awake()
     {
-        GameObject Player1, player2;
-        public float ZoomSmootherner;
+        
     }
 
+    
+
+    public static float getDistanceBetween(GameObject gameobject1, GameObject gameobject2)
+    {
+        return Vector3.Distance(gameobject1.transform.position, gameobject2.transform.position);
+    }
+    public static float getDistanceBetween(Transform Transform1, Transform Transform2)
+    {
+        return Vector3.Distance(Transform1.position, Transform2.position);
+    }
 }
