db5387c39dcad878d53abb92134bd145763bc9b7
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2019-12-04 08:52:34 +0100

    no message

---
+++ Assets/Stablize.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stablize : MonoBehaviour
+{
+    Transform head;
+    // Start is called before the first frame update
+    void Start()
+    {
+        head = GetComponentInParent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(head.forward);
+        transform.rotation = Quaternion.LookRotation(head.forward);
+        Debug.DrawRay(transform.position, transform.forward, Color.green);
+
+    }
+}
