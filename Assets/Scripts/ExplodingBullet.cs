260692fb0c3f2baf6a3a7841653933a414495101
Johnny.Chu <johnny.chu@elev.ga.lbs.se>
2019-12-18 10:16:09 +0100

    no message

---
+++ Assets/Scripts/ExplodingBullet.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodingBullet : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float thrust = 10f;
+    public bool createrForce = true;
+    public bool shouldExplode = false;
+    public float lifeTime = 10f;
+
+    public float power = 10f;
+    public float radius = 5f;
+    public float upwardThrust = 3f;
+    public GameObject explosion;
+    private Collider[] overlappers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (createrForce)
+        {
+            rb.AddForce(transform.forward * thrust);
+            createrForce = false;
+        }
+        if (shouldExplode)
+        {
+            Collider[] colliders = GetOverlappers();
+
+            foreach(Collider hit in colliders)
+            {
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(power, this.transform.position, radius, upwardThrust);
+                }
+            }
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        shouldExplode = true;
+        Instantiate(explosion, this.transform.position, this.transform.rotation);
+    }
+
+    Collider[] GetOverlappers()
+    {
+        return Physics.OverlapSphere(this.transform.position, radius);
+    }
+}
