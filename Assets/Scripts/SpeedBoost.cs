6bceb55c9913176b3554ac49b175f3676ceeedee
Johnny.Chu <johnny.chu@elev.ga.lbs.se>
2020-01-08 09:06:37 +0100

    no message

--- Assets/Scripts/SpeedBoost.cs
+++ Assets/Scripts/SpeedBoost.cs
@@ -6,19 +6,30 @@ public class SpeedBoost : MonoBehaviour
 {
     TankMovement movement_speed;
     public float speedBost = 10;
+    public float duration = 5;
+    public float currentDuration;
 
     private void Awake()
     {
         
     }
 
-
-    public void OnTriggerEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             TankMovement.i.movement_speed += speedBost;
+            currentDuration = duration;
+            currentDuration =- Time.deltaTime;
             Destroy(gameObject);
+        }
+    }
+
+    public void timer()
+    {
+        if (currentDuration < 0)
+        {
+            TankMovement.i.movement_speed -= speedBost;
         }
     }
 }
