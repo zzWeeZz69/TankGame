c1cdaec50c7026393de69a8c27c6b4fb095ca947
Sebastian Eking <sebastian.eking@elev.ga.lbs.se>
2020-01-13 17:43:29 +0100

    Main Menu Scripts and Scenes

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
